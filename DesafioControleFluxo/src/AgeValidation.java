15eb3a7ad34b4760aa3407985e75cd65af9f8245
FelipeFerraz4 <felipeferraz2402@gmail.com>
2024-05-01 11:03:41 -0300

    dio challenge java 1

---
+++ DesafioControleFluxo/src/AgeValidation.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class AgeValidation {
+    public static void main(String[] args) { 
+        Scanner scanner = new Scanner(System.in); 
+
+        int age = scanner.nextInt();
+
+        if (age >= 18) {
+            System.out.println("Voce esta elegivel para criar uma conta bancaria.");
+        } else {
+            System.out.println("Voce nao esta elegivel para criar uma conta bancaria.");
+        }
+
+        scanner.close(); 
+    }
+}
